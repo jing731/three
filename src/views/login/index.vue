d1cd5e530fec2c1a6bb0f284fae8f2f87556bbb4
jing <3223191529@qq.com>
2020-05-28 19:24:52 +0800

    登录模块，样式处理完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar
+    class="app-nav-warp"
+    title="注册/登录"
+    left-arrow
+    @click-left="$router.back()"
+    />
+    <!-- 导航栏end -->
+    <!-- 输入框 -->
+    <van-cell-group>
+  <van-field
+    v-model="user.mobile"
+    icon-prefix='icon'
+    left-icon="shouji"
+    placeholder="请输入手机号"
+  />
+  <van-field
+    v-model="user.code"
+    clearable
+    icon-prefix='icon'
+    left-icon="yanzhengma"
+    placeholder="请输入验证码"
+  >
+   <template #button>
+    <van-button size="small" class="send" round>发送验证码</van-button>
+   </template>
+  </van-field>
+   </van-cell-group>
+    <!-- 输入框end -->
+    <!-- 按钮 -->
+    <div class="app-btn">
+      <van-button type="info" class="app-send"  block>登录</van-button>
+    </div>
+    <!-- 按钮end -->
+  </div>
 </template>
 <script>
 export default {
@@ -7,15 +42,35 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+  }
 }
 </script>
-<style lang="scss" scoped>
-
+<style lang="less" scoped>
+.app-btn{
+  padding: 26px 16px;
+  .app-send{
+    background: #6db4fb;
+    border: none;
+    font-size: 16px;
+  }
+}
+.send{
+  background: #ededed;
+  color: #666666;
+  font-size: 12px;
+  width: 80px;
+  height: 30px;
+}
 </style>
