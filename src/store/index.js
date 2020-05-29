1a845411f975ecfb34dc6997d1b7a88ec251884f
jing <3223191529@qq.com>
2020-05-29 09:29:56 +0800

    token数据封装完成

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { setItem, getItem } from '@/store/storage'
 Vue.use(Vuex)
-
+const USER_KEY = 'user'
 export default new Vuex.Store({
   state: {
-    user: JSON.parse(window.localStorage.getItem('user')) // 用户的token等数据
+    user: getItem(USER_KEY)
+    // user: JSON.parse(window.localStorage.getItem('user')) // 用户的token等数据
   },
   mutations: {
     setUser (state, data) {
       state.user = data
+      setItem(USER_KEY, state.user)
       // 由于容器刷新，数据就消失，所以持久化到本地
-      window.localStorage.setItem('user', JSON.stringify(this.state.user))
+      // window.localStorage.setItem('user', JSON.stringify(this.state.user))
     }
   },
   actions: {
