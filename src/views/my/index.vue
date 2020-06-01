0e07f4973610e66d66cb0e672dc377b5935182bf
jing <3223191529@qq.com>
2020-06-01 15:03:29 +0800

    基本样式完成

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="my-container">
-    <div class="user-container">
-       <van-cell-group>
+       <van-cell-group class="user-container">
   <van-cell
+  class="base-info"
   title="单元格"
   value="内容"
   center
+  :border='false'
   >
     <div slot="icon">
       <van-image
-      width="50"
-      height="50"
+      class="avator"
       round
       fit='cover'
       src="https://img.yzcdn.cn/vant/cat.jpeg" />
       </div>
-      <div slot="title">昵称</div>
+      <div slot="title"
+      class="name">昵称</div>
       <van-button
       round
-      size='small'>编辑按钮</van-button>
+      size='small'
+      class="btn">编辑按钮</van-button>
   </van-cell>
   </van-cell-group>
-  <van-grid>
+  <van-grid :border='false'>
   <van-grid-item>
     <div slot="text">
       <div class="icon">123</div>
@@ -47,8 +49,7 @@
     </div>
   </van-grid-item>
  </van-grid>
-    </div>
-    <van-grid :column-num="2">
+    <van-grid class="btn-4" :column-num="2">
   <van-grid-item
   icon-prefix='icon'
   icon="shoucang"
@@ -60,8 +61,8 @@
  </van-grid>
  <!-- 页面导航 -->
  <van-cell title="消息通知" is-link to="" />
- <van-cell title="小智同学" is-link to="" />
- <van-cell title="退出登录" is-link to="" />
+ <van-cell class="btn-4" title="小智同学" is-link to="" />
+ <van-cell class="logout" title="退出登录"  />
   </div>
 </template>
 <script>
@@ -78,3 +79,41 @@ export default {
   methods: {}
 }
 </script>
+<style lang="less" scoped>
+.my-container{
+.user-container{
+  background: url("./banner.png") no-repeat;
+  background-size: cover;
+  .base-info{
+    background-color: unset;
+    .avator{
+      width: 66px;
+      height: 66px;
+      border: 1px solid #ffffff;
+      box-sizing: border-box;
+      margin-right: 10px;
+    }
+    .name{
+      color: #ffffff;
+      font-size: 15px;
+    }
+    .btn{
+      height: 16px;
+      color: #666666;
+      font-size: 10px;
+    }
+  }
+}
+/deep/ .van-grid-item__content{
+    background-color: unset;
+  }
+  .logout{
+    text-align: center;
+    color: tomato;
+  }
+  .btn-4{
+    margin-bottom: 4px;
+  }
+}
+
+</style>
